ba894f3c904f59c006b0c5316a6badd99d718136
Vitoria <vazevedo73@gmail.com>
2024-02-20 17:26:00 -0300

    feito

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'amarelo': '#F3CC4D',
+        'marrom': '#2E1012'
+      },
+      fontFamily: {
+        font: ['DM Serif Display']
+      }
+    },
+  },
+  plugins: [],
+}
+
